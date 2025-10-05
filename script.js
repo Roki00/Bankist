caae3dc6b581d603b093d44c46b50e94ed4e5fb6
Eron <eronblakaj98@gmail.com>
2025-10-05 20:31:30 +0200

    updated the reduce method

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ calcDisplayBalance(account1.movements);
 //   ['GBP', 'Pound sterling'],
 // ]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -198,3 +198,13 @@ calcDisplayBalance(account1.movements);
 // const balance = movements.reduce((acc, cur) => acc + cur, 0);
 
 // console.log(balance);
+
+//Maximum value
+
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) {
+    return acc;
+  } else {
+    return mov;
+  }
+}, movements[0]);
